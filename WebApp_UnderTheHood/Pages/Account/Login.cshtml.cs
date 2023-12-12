222f3a48aa865bd850a43653070bdf561025bb02
Thilakshan Baskaran <telakshanb@gmail.com>
2023-12-12 08:47:27 -0500

    first commit

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using WebApp_UnderTheHood.Extensions;
+
+namespace WebApp_UnderTheHood.Pages.Account;
+
+public class LoginModel : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; } = new Credential();
+
+    private Credential defaultCredentials = new Credential
+    {
+        UserName = "admin",
+        Password = "JAvlGPq9JyTdtvBO6x2llnRI1+gxwIyPqCKAn3THIKk="
+    };
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        if (Credential.UserName == "admin" && Credential.Password.Sha256() == defaultCredentials.Password)
+        {
+            //creating the security context
+            var claims = new List<Claim> 
+            { 
+                new Claim(ClaimTypes.Name, "admin") ,
+                new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+            };
+
+            var identity = new ClaimsIdentity(claims, "wepapp_cookie");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync("wepapp_cookie", claimsPrincipal);
+
+            return RedirectToPage("/Index");
+        }
+
+        return Page();
+    }
+}
+
+public class Credential
+{
+    [Required]
+    [Display(Name = "Username")]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
