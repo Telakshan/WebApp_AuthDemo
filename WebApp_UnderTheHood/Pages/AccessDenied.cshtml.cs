64278eb75d69dbb368c5a2b7197e0ea1d22eccac
telakshan <telakshanb@gmail.com>
2024-02-12 10:00:36 -0500

    litttle cleanup

--- WebApp_UnderTheHood/Pages/AccessDenied.cshtml.cs
+++ WebApp_UnderTheHood/Pages/AccessDenied.cshtml.cs
@@ -1,12 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApp_AuthDemo.Pages
+namespace WebApp_AuthDemo.Pages;
+
+public class AccessDeniedModel : PageModel
 {
-    public class AccessDeniedModel : PageModel
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
 }
